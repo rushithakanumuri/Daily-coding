249d64ad1305a766618fedbfba4dab1ada350d20
rushithakanumuri <99245826+rushithakanumuri@users.noreply.github.com>
2022-06-20 22:21:44 +0530

    Add files via upload

---
+++ prob_37.py
@@ -0,0 +1,10 @@
+The power set of a set is the set of all its subsets. Write a function that, given a set, generates its power set.
+
+For example, given the set {1, 2, 3}, it should return {{}, {1}, {2}, {3}, {1, 2}, {1, 3}, {2, 3}, {1, 2, 3}}.
+
+
+def power_set(s):
+    if not s:
+        return [[]]
+    result = power_set(s[1:])
+    return result + [subset + [s[0]] for subset in result]
