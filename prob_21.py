982168cbab7bd6474da74cd792c3ff4818837e3c
rushithakanumuri <99245826+rushithakanumuri@users.noreply.github.com>
2022-06-21 23:29:26 +0530

    Update and rename minimumrooms.py to prob_21.py

---
+++ prob_21.py
@@ -0,0 +1,21 @@
+Given an array of time intervals (start, end) for classroom lectures (possibly overlapping), find the minimum number of rooms required.
+
+For example, given [(30, 75), (0, 50), (60, 150)], you should return 2.
+
+
+def max_overlapping(intervals):
+    starts = sorted(start for start, end in intervals)
+    ends = sorted(end for start, end in intervals)
+
+    current_max = 0
+    current_overlap = 0
+    i, j = 0, 0
+    while i < len(intervals) and j < len(intervals):
+        if starts[i] < ends[j]:
+            current_overlap += 1
+            current_max = max(current_max, current_overlap)
+            i += 1
+        else:
+            current_overlap -= 1
+            j += 1
+    return current_max
