c39578b5505cced85a1e21d19b166ff0785714d3
rushithakanumuri <99245826+rushithakanumuri@users.noreply.github.com>
2022-06-22 23:57:53 +0530

    Add files via upload

---
+++ prob_36.py
@@ -0,0 +1,25 @@
+
+Given the root to a binary search tree, find the second largest node in the tree.
+
+
+
+def second_largest(root):
+    def inorder(node):
+        if not node or count[0] == 2:
+            return
+
+        if node.right:
+            inorder(node.right)
+
+        count[0] += 1
+        if count[0] == 2:
+            val.append(node.val)
+            return
+
+        if node.left:
+            inorder(node.left)
+
+    count = [0]
+    val = []
+    inorder(root)
+    return val[0]
